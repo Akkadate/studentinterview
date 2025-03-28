42fce7afe44803b6316fe1dc4857d39cde8329fd
Mydevth <mydevth@chonburi.biz>
2025-03-28 11:11:33 +0700

    fix font 2

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,7 +1,7 @@
 // frontend/src/app/layout.jsx
 "use client";
 
-import { Inter } from "next/font/google";
+import { Inter, Prompt } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { InterviewProvider } from "../contexts/InterviewContext.jsx";
